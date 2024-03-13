fe2cc27ab8b32070c056b83513ed01286995e382
suelenschmidt <109631070+suelenschmidt@users.noreply.github.com>
2024-03-13 15:50:38 -0300

    Add files via upload

---
+++ sequenciaFibonacci.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class sequenciaFibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe um número para verificar se pertence à sequência de Fibonacci: ");
+        int numero = scanner.nextInt();
+
+        if (verificaFibonacci(numero)) {
+            System.out.println(numero + " pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println(numero + " não pertence à sequência de Fibonacci.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean verificaFibonacci(int num) {
+        int a = 0, b = 1;
+        while (b < num) {
+            int temp = b;
+            b = a + b;
+            a = temp;
+        }
+        return b == num;
+    }
+}
